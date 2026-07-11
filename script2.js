104d3de68fdbd82ebdc835ddb3e1ee723615f2d1
Linwelss <Lina-Melissa@web.de>
2026-07-11 23:19:47 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,19 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var cvButton = document.querySelector('.btn-primary');
-  if (cvButton) {
-    cvButton.addEventListener('click', function () {
-      // Später: Link zur echten CV-PDF einsetzen
-      window.open('assets/lina-melissa-cv.pdf', '_blank');
-    });
-  }
 
-  var aboutButton = document.querySelector('.btn-secondary');
-  if (aboutButton) {
-    aboutButton.addEventListener('click', function () {
-      var aboutSection = document.querySelector('#about');
-      if (aboutSection) {
-        aboutSection.scrollIntoView({ behavior: 'smooth' });
-      }
-    });
-  }
-});
